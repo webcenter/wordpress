d5675f34803d366cbc8f134cc478242a2ebc1489
Henrique Monteiro Junior <hmjsite@gmail.com>
2017-07-24 10:36:30 -0300

    get url thumbnail

--- index.php
+++ index.php
@@ -14,3 +14,10 @@
 <?php } else { ?>
     <!-- imagem padrao -->
 <?php } ?>
+
+<!--GET URL THUMBNAIL-->
+<?php if (has_post_thumbnail()) : ?>
+    <img src="<?php $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'size', false, ''); echo $src[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" class="img-responsive"/>
+<?php else : ?>
+    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/thumb.jpg">
+<?php endif; ?>
